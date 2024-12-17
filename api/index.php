44b710c2f247240bc2d5da5be6b771984c1147ab
Basil Suter <git@nadar.io>
2024-12-17 19:38:41 +0000

    adjust vercel

---
+++ api/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Here is the serverless function entry
+ * for deployment with Vercel. If you dont
+ * use Vercel, you can remove this folder
+ * and file safely.
+ */
+require __DIR__.'/../public/index.php';
